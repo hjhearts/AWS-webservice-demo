5edae9ae3ed32bf4ca75c30044c31c7c8541c3a9
hjhearts <hjhearts@nate.com>
2020-12-25 19:14:43 +0900

    git ignore

---
+++ src/main/java/com/aws/demo/springboot/web/PostsApiController.java
@@ -0,0 +1,21 @@
+package com.aws.demo.springboot.web;
+
+import com.aws.demo.springboot.service.posts.PostsService;
+import com.aws.demo.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+
+}
