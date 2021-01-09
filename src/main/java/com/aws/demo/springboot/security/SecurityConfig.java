ddaecd0f0481a0f674da3c9719fd6de3e644626e
hjhearts <hjhearts@nate.com>
2021-01-09 20:46:18 +0900

    Security

---
+++ src/main/java/com/aws/demo/springboot/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.aws.demo.springboot.security;
+
+import com.aws.demo.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@SpringBootConfiguration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/js/**", "/images/**", "/h2-console").permitAll()
+                .antMatchers("/api/v1/**").hasAnyRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
+    }
+}
--- src/main/java/com/aws/demo/springboot/web/IndexController.java
+++ src/main/java/com/aws/demo/springboot/web/IndexController.java
@@ -1,25 +1,42 @@
 package com.aws.demo.springboot.web;
 
+import com.aws.demo.springboot.security.dto.SessionUser;
 import com.aws.demo.springboot.service.posts.PostsService;
+import com.aws.demo.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final HttpSession httpSession;
     private final PostsService postsService;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+        if(user != null){
+            model.addAttribute("loginUserName", user.getName());
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable("id") Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
